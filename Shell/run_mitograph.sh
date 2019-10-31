94ca2689f6b4fcae3d2d55220914e87e1e96cb20
granthussey <16183823+granthussey@users.noreply.github.com>
2019-10-31 16:22:45 -0400

    Update run_mitograph.sh

--- Shell/run_mitograph.sh
+++ Shell/run_mitograph.sh
@@ -28,6 +28,6 @@ echo "Current Slurm_Array_Task_ID: " $SLURM_ARRAY_TASK_ID
 ~/bin/MitoGraph -xy 0.0973499 -z 0.2 -scales 1.0 1.3 4 -adaptive 10 -path ${DIRS[$SLURM_ARRAY_TASK_ID]}
 
 echo "Now moving all files to " ${DIRS[-1]}
-cp -r ${DIRS[$SLURM_ARRAY_TASK_ID]} ${DIRS[-1]}
+find ${DIRS[$SLURM_ARRAY_TASK_ID]} -type f -print0 | xargs -0 cp -t ${DIRS[-1]}
 
 date
