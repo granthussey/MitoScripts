8719fe48e232cc634f0469413c4387330ca0b01a
granthussey <16183823+granthussey@users.noreply.github.com>
2019-11-06 10:21:56 -0500

    Create rename_KRAS_to_p53.py

---
+++ extra_scripts/rename_KRAS_to_p53.py
@@ -0,0 +1,23 @@
+
+import os
+
+
+# 1. Rename KRAS to XXX
+# 2. Rename p53 to YYY
+# 3. Rename XXX to p53
+# 4. Rename YYY to KRAS
+
+def rename_files_in_directory(dir):
+
+    cur_names = os.listdir(dir)
+
+
+    # First, we copy what the current names are.
+    new_names = cur_names
+
+    # Second, we edit that list with the new names, indexed in the same way.
+    new_names =  new_names.replace('KRAS','XXX').replace('p53','YYY').replace('YYY','KRAS').replace('XXX','p53')
+
+
+    for iter in len(cur_names):
+        os.rename(cur_names[iter], new_names[iter])
