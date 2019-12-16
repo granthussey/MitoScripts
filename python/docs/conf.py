b03f615fd149e26b76acf6f7c411f6ee9446327d
granthussey <16183823+granthussey@users.noreply.github.com>
2019-12-16 11:56:57 -0500

    fixing sphinx

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -10,22 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sys #uncomment
-import os #uncomment
-import sphinx_rtd_theme #add
-sys.path.insert(0, os.path.abspath("../src/<package_name>")) #update
-extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"] #update
-html_theme = "sphinx_rtd_theme" # Change
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'MitoScripts'
-copyright = '2019, Grant Alfred Hussey'
-author = 'Grant Alfred Hussey'
+project = "MitoProcess"
+copyright = "2019, Grant Hussey"
+author = "Grant Hussey"
 
 # The full version, including alpha/beta/rc tags
-release = 'beta'
+release = "beta"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,16 +30,15 @@ release = 'beta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -50,9 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="To assist in quantifying mitochondrial morphology",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/granthussey/MitoScripts",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
