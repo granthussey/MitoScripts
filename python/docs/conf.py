444dfb06df1ce7be7b81d8ac3051935e5dc92d1d
granthussey <16183823+granthussey@users.noreply.github.com>
2019-12-16 12:10:42 -0500

    Update conf.py

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -10,14 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import sys #uncomment
+import os #uncomment
+import sphinx_rtd_theme #add
+sys.path.insert(0, os.path.abspath("../src/mitoscripts")) #update
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"] #update
+html_theme = "sphinx_rtd_theme" # Change
 
 # -- Project information -----------------------------------------------------
 
-project = "MitoProcess"
+project = "MitoScripts"
 copyright = "2019, Grant Hussey"
 author = "Grant Hussey"
 
@@ -30,8 +32,6 @@ release = "beta"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
@@ -46,8 +46,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
